5ca46e239364288d26eeece93714747ac4dec60c
Aldren Martin <113560992+AldrenMartin@users.noreply.github.com>
2022-10-11 03:52:59 +0000

    I am going tomorrow morning to a tutor to try and solve the error on the code.

--- Main.java
+++ Main.java
@@ -1,33 +1,53 @@
-import java.util.Scanner;
+    /*import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    //Ask the user here
-	  Stable s = new Stable();
-		System.out.println("What is the address of the stable?");
-		Scanner scanner = new Scanner(System.in);
-		s.address = scanner.nextLine();
-		do {
-			System.out.println("Do you wish to add a horse to the stable?"); 
-			char input = scanner.next().charAt(0);
-			scanner.nextLine();
-			if(input == 'y'){
-				Horse h;
-				System.out.println("What is the name of the horse?");
-				String hn = scanner.nextLine();
-				System.out.println("What is the weight of the horse?");
-				double w = scanner.nextDouble();
-				System.out.println("Enter true if the horse is tame or false if it is not.");
-				boolean t = scanner.nextBoolean();
-				h = new Horse(hn, w, t);
-				s.arrayList.add(h);
-			}//testing goodest 4
-			else{
-				break;
-			}
-		}while(true);
-		//Display the info here
-                //You code here!
-	  }
+
+class main {
+  public static void main(String[]args){
+
+   // Horses Horsess = new Horses();
+    Stable Stabble = new Stable();
+    Scanner in = new Scanner(System.in);
+
+      System.out.println("What is the address of the stable?");
+        Stabble.address = in.nextLine();
+    
+    do{
+      System.out.println("Do you wish to add a horse to the stable?");
+        Stabble.input = in.next().charAt(0);
+
+    if(Stabble.input == 'y'){
+        
+      System.out.println("What is the name of the horse?");
+          //Horsess.name = in.nextLine();
+          String name = in.nextLine();
+      
+      System.out.println();
+      
+        System.out.println("What is the weight of your horse?");
+          //Horsess.weight = in.nextDouble();
+          Double weight = in.nextDouble();
+      
+        System.out.println("Enter true if the horse is tame or false if it is not.");
+         // Horsess.tame = in.nextBoolean();
+          boolean tame = in.nextBoolean();
+
+      Horses h = new Horses (name, weight, tame);
+   
+      Stabble.HorsesInfo.add(h);
+      
+    }
+      else {
+        break;
+      }
+    
+    for (int i = 0; i < Stabble.HorsesInfo.size(); i++){
+
+      System.out.println(Stabble.HorsesInfo.get(1));
+    }
+    
+    } while(true);
   }
+  
 }
+
+ */
